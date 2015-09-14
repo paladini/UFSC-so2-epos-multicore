985dd02095ea07b859d78ca43b1db8b4716ae5d8
Ranieri Althoff <ranisalt@gmail.com>
2015-09-14 01:31:49 -0300

    Remove extra test at mutex

--- src/abstraction/mutex.cc
+++ src/abstraction/mutex.cc
@@ -23,8 +23,7 @@ void Mutex::lock()
 
     begin_atomic();
     if(tsl(_locked))
-        while(tsl(_locked))
-            sleep(); // implicit end_atomic()
+        sleep(); // implicit end_atomic()
     else
         end_atomic();
 }
